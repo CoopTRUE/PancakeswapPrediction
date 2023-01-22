5335e0b181105072a1993056c729fa44804ba4ec
coopTRUE <coopdog4545@gmail.com>
2023-01-22 12:29:39 -0800

    added abi and starter code

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,10 @@ dotenv.config()
 import { ethers } from 'ethers'
 import ABI from './ABI.json'
 
+const CONTRACT_ADDRESS = '0x18B2A687610328590Bc8F2e5fEdDe3b582A49cdA'
+
 const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
-const wallet = new ethers.Wallet('0x...', provider)
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, wallet)
+
+console.log(contract)
